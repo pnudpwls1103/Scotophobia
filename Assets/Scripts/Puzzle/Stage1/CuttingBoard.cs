67e7c0341eb6835d53e49d9e2ba60b57d6b6ebaa
taejeong <sontaewjd@nate.com>
2022-11-11 23:01:21 +0900

    feat:  요리 퍼즐 재료 옮길 시 사진 올라가게
    도마에 올리면 재료가 쌓이고
    후라이팬에 올리면 일정시간 조리된 후 재료창에 조리된 재료 들어감

--- Assets/Scripts/Puzzle/Stage1/CuttingBoard.cs
+++ Assets/Scripts/Puzzle/Stage1/CuttingBoard.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CuttingBoard : MonoBehaviour
 {
@@ -9,13 +10,47 @@ public class CuttingBoard : MonoBehaviour
     [SerializeField]
     CookPuzzle cookPuzzle;
     public int order = 0;
+
+    List<GameObject> ingredients = new List<GameObject>();
+    [SerializeField]
+    Vector2 ingredientPos;
+    [SerializeField]
+    float size;
+    int sortingOrder = 4;
     public void Cook(GameObject go)
     {
+        PutIngredient(go);
         menu.Delete(go.name);
         order++;
         if (order == 2)
+        {
             menu.Insert("ButterBread");
+            DeleteAllIngredient();
+        }
         else if (order == 5)
+        {
             cookPuzzle.Clear();
+            DeleteAllIngredient();
+        }
+    }
+
+    void PutIngredient(GameObject go)
+    {
+        GameObject ingredient = new GameObject();
+        ingredient.transform.position = ingredientPos;
+        ingredient.transform.localScale = new Vector3(1, 1, 1) * size;
+        ingredient.name = go.name;
+        SpriteRenderer sr = ingredient.AddComponent<SpriteRenderer>();
+        sr.sprite = go.GetComponent<Image>().sprite;
+        sr.sortingOrder = sortingOrder;
+        sortingOrder++;
+        ingredients.Add(ingredient);
+    }
+
+    void DeleteAllIngredient()
+    {
+        foreach (GameObject ingredient in ingredients)
+            Destroy(ingredient);
+        ingredients.Clear();
     }
 }
--- Assets/Scripts/Puzzle/Stage1/FryingPan.cs
+++ Assets/Scripts/Puzzle/Stage1/FryingPan.cs
@@ -1,19 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FryingPan : MonoBehaviour
 {
     public int order = 0;
     [SerializeField]
     IngredientMenu menu;
+
+    List<GameObject> ingredients = new List<GameObject>();
+    [SerializeField]
+    Vector2 ingredientPos;
+    [SerializeField]
+    float size;
+    [SerializeField]
+    float watingTime;
+    int sortingOrder = 4;
     public void Cook(GameObject go)
     {
+        PutIngredient(go);
         menu.Delete(go.name);
         order++;
         if (order == 1)
-            menu.Insert("CookedBread");
+            StartCoroutine(MainCook("CookedBread"));
         else if (order == 2)
-            menu.Insert("CookedEgg");
+            StartCoroutine(MainCook("CookedEgg"));
+    }
+
+    
+
+    IEnumerator MainCook(string name)
+    {
+        yield return new WaitForSeconds(watingTime);
+        menu.Insert(name);
+        DeleteAllIngredient();
+    }
+
+    void PutIngredient(GameObject go)
+    {
+        GameObject ingredient = new GameObject();
+        ingredient.transform.position = ingredientPos;
+        ingredient.transform.localScale = new Vector3(1, 1, 1) * size;
+        ingredient.name = go.name;
+        SpriteRenderer sr = ingredient.AddComponent<SpriteRenderer>();
+        sr.sprite = go.GetComponent<Image>().sprite;
+        sr.sortingOrder = sortingOrder;
+        sortingOrder++;
+        ingredients.Add(ingredient);
+    }
+
+    void DeleteAllIngredient()
+    {
+        foreach (GameObject ingredient in ingredients)
+            Destroy(ingredient);
+        ingredients.Clear();
     }
 }
