0f859485d9b318680ab9c5b02fe063e54d803593
Yejin Min <pnudpwls1103@pusan.ac.kr>
2022-11-22 00:20:23 +0900

    [Feat] 상호작용 방식을 마우스 좌클릭으로 수정_복원

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -15,7 +15,9 @@ public class PlayerController : MonoBehaviour
     public static event System.Action OnBulbOn = null;
     public static event System.Action OnBulbOff = null;
 
+    public float maxDistance = 9f;
     public GameObject scanObject;
+    public GameObject scanClickObject;
     
     Rigidbody2D rigid;
     private SkeletonAnimation skeletonAnimation;
@@ -33,8 +35,6 @@ public class PlayerController : MonoBehaviour
     {
         Move();
 
-        if (Input.GetMouseButtonDown(0))
-            Interact();
         if (Input.GetKeyDown(KeyCode.I))
             UI_Root.TogglePopup(typeof(Define.UI_Popup), (int)Define.UI_Popup.Inventory);
         if (Input.GetKeyDown(KeyCode.O))
@@ -49,6 +49,9 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         ScanObject();
+        GetScanObjectMouse();
+        if (scanClickObject)
+            Interact();
     }
 
     private void ScanObject()
@@ -66,16 +69,34 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private void GetScanObjectMouse()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Ray2D ray= new Ray2D(pos, Vector2.zero);
+
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity, LayerMask.GetMask("Object"));
+
+            if(hit.collider != null)
+            {
+                float distance = Vector2.Distance(hit.transform.position, transform.position);
+                if(distance <= maxDistance)
+                    scanClickObject = hit.collider.gameObject;
+                return;
+            } 
+        }
+
+        scanClickObject = null;
+    }
     void Interact()
     {
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 1f);
-        foreach (Collider2D col in colliders)
-            if (col.GetComponent<IInteraction>() != null)
-            {
-                IInteraction inter = col.GetComponent<IInteraction>();
-                inter.Interact(gameObject);
-                return;
-            }
+        Collider2D col = scanClickObject.GetComponent<Collider2D>();
+        if (col.GetComponent<IInteraction>() != null)
+        {
+            IInteraction inter = col.GetComponent<IInteraction>();
+            inter.Interact(gameObject);
+        }
     }
     void ToggleBulb()
     {
